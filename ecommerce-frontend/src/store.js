17785eb36dc0e0dc0a3af5b69a9ceb3eb0069916
IanGichengo <iangatimu6@gmail.com>
2024-10-24 00:45:03 -0400

    ecommerce_app

--- ecommerce-frontend/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './features/cartSlice'; // Import your cart slice
-
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer, // Add your reducers here
-  },
-});
