e20871a000695181c1f5b9c562efc9f5cb7d367c
IanGichengo <iangatimu6@gmail.com>
2024-10-27 09:28:43 -0400

    ecommerce_app

--- ecommerce-frontend/src/components/Footer.js
+++ ecommerce-frontend/src/components/Footer.js
@@ -3,12 +3,10 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="logo">Organic Farm Fresh by Sokoni</div>
-      <div className="contact-info">
-        <p>Contact Us: +123 456 7890 | sokoni@organicfarm.com</p>
-        <p>&copy; 2024 Sokoni. All rights reserved.</p>
-      </div>
+    <footer>
+	  <div class="row">
+	  <div class="col">
+	  <
     </footer>
   );
 }
