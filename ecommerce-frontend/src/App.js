604448c937090b55a27344a2fe04d88bdf6d1668
Ian Gichengo <iangatimu6@gmail.com>
2024-10-31 07:25:20 +0300

    new_collections

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
--- ecommerce-frontend/src/components/Popular/Popular.jsx
+++ ecommerce-frontend/src/components/Popular/Popular.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Popular.css'
 import data_product from '../Assets/data'
 
-export const Popular = () => {
+const Popular = () => {
     return (
     <div className='popular'>
         <h1>POPULAR IN WOMEN</h1>
