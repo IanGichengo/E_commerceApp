91ce5e60f62ee4676a03a5921ce05639817ba2f6
Ian Gichengo <iangatimu6@gmail.com>
2024-10-31 09:39:20 +0300

    update

--- ecommerce-frontend/src/components/Hero/Hero.jsx
+++ ecommerce-frontend/src/components/Hero/Hero.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import './Hero.css'
-import hand_icon from '../Assets/hand_icon.png'
-import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/hero_image.png'
+import React from 'react';
+import './Hero.css';
+import hand_icon from '../Assets/hand_icon.png';
+import arrow_icon from '../Assets/arrow.png';
+import hero_image from '../Assets/hero_image.png';
 
 export const Hero = () => {
     return (
-    <div className='hero'>
-        <div className="Hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
-            <div>
+        <div className='hero'>
+            <div className="hero-left">
+                <h2>NEW ARRIVALS ONLY</h2>
                 <div className="hero-hand-icon">
                     <p>new</p>
-                    <img src={hand_icon} alt="" />
+                    <img src={hand_icon} alt="Hand Icon" />
                 </div>
                 <p>collections</p>
                 <p>for everyone</p>
+                <div className="hero-latest-btn">
+                    <div>Latest Collection</div>
+                    <img src={arrow_icon} alt="Arrow Icon" />
+                </div>
             </div>
-            <div className="hero-latest-btn">
-                <div>Latest Collection</div>
-                <img src={arrow_icon} alt="" />
+            <div className="hero-right">
+                <img src={hero_image} alt="Hero" />
             </div>
         </div>
-        <div className="Hero-right">
-            <img src={hero_image} alt="" />
-        </div>
-    </div>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
--- ecommerce-frontend/src/components/NewsLetter/NewsLetter.jsx
+++ ecommerce-frontend/src/components/NewsLetter/NewsLetter.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './NewsLetter.css'
+import React from 'react';
+import './NewsLetter.css';
 
 const NewsLetter = () => {
     return (
-    <div className='newsletter'>
-        <h1>Get Exclusive Offers On Your Email</h1>
-        <p>Subscribe to our newsletter and stay updated</p>
-        <div>
-            <input type="email" placeholder='Your Email id' />
-            <button>Subscribe</button>
+        <div className='newsletter'>
+            <h1>Get Exclusive Offers On Your Email</h1>
+            <p>Subscribe to our newsletter and stay updated</p>
+            <div className="input-container">
+                <input type="email" placeholder='Your Email id' />
+                <button>Subscribe</button>
+            </div>
         </div>
-    </div>
-    )
+    );
 }
 
-export default NewsLetter
+export default NewsLetter;
