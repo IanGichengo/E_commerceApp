85382ef651bf9e4d579636d65483216b15e67af1
Ian Gichengo <iangatimu6@gmail.com>
2024-11-11 14:30:47 +0300

    update

--- ecommerce-frontend/src/components/Hero/Hero.jsx
+++ ecommerce-frontend/src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
     return (
         <div className='hero'>
             <div className="hero-left">
-                <h2>NEW ARRIVALS ONLY</h2>
+                <h2></h2>
                 <div className="hero-hand-icon">
                     <p>new</p>
                     <img src={hand_icon} alt="Hand Icon" />
