fc251a78e7af0f8ff2e354a5bd8a0b636f188e39
Ian Gichengo <iangatimu6@gmail.com>
2024-11-21 10:47:56 +0300

    homepage

--- ecommerce-frontend/src/components/Hero/Hero.jsx
+++ ecommerce-frontend/src/components/Hero/Hero.jsx
@@ -2,24 +2,20 @@ import React from 'react';
 import './Hero.css';
 import arrow_icon from '../Assets/arrow.png';
 import hero_image from '../Assets/hero_image.png';
-import homepage_background from '../Assets/homepage_background.jpg';
 
 export const Hero = () => {
     return (
-        <div className='hero'>
-            <div>
-                <img src={homepage_background} alt="Hero" />
-            </div>
+        <div className="hero">
             <div className="hero-left">
                 <p>collections</p>
                 <p>for everyone</p>
                 <div className="hero-latest-btn">
                     <div>Latest Collection</div>
-                    <img src={arrow_icon} alt="Arrow Icon" />
+                    <img src={arrow_icon} alt="Arrow pointing to the latest collection" />
                 </div>
             </div>
             <div className="hero-right">
-                <img src={hero_image} alt="Hero" />
+                <img src={hero_image} alt="Decorative hero representation" />
             </div>
         </div>
     );
