69defd4e4b492958ed9967974a79c37b9471b744
IanGichengo <iangatimu6@gmail.com>
2024-11-23 07:29:31 -0500

    counter_navbar

--- ecommerce-frontend/src/Components/Navbar/Navbar.jsx
+++ ecommerce-frontend/src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
-import cart_icon from '../Assets/cart_icon.png';
 
 const Navbar = () => {
     const [menu, setMenu] = useState("shop");
@@ -34,10 +33,6 @@ const Navbar = () => {
                 <Link to='/login'>
                     <button>Login</button>
                 </Link>
-                <Link to='/cart'>
-                    <img src={cart_icon} alt="" />
-                </Link>
-                <div className="nav-cart-count">0</div>
             </div>
         </div>
     );
